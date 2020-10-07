e95cf573e1651654a0c2db6acb5626de5d05b3ee
cldaly <clayton.daly91@gmail.com>
2020-10-07 16:07:39 -0400

    begin work on project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
 
 class App extends Component {
-  render() {
-    return(<div>Clayton Codes</div>);
-  }
+	render() {
+		return(
+			<div className='clayton-codes'>
+				<Header />
+				Clayton Codes
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
